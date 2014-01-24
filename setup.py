b8868fe34e3b4e94c0e26705f972d79235814f75
Felix Kaiser <felix.kaiser@fxkr.net>
2014-01-25 00:03:23 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='yamlcheck',
+    version='0.1.0',
+    description='Check if files adhere to YAML syntax',
+    author='Felix Kaiser',
+    author_email='felix.kaiser@fxkr.net',
+    long_description=open('README.md').read(),
+    license=open('LICENSE').read(),
+    url='https://github.com/fxkr/yamlcheck',
+    packages=[],
+    scripts=['yamlcheck'])
+
